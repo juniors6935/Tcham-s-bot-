b75845ace070023e5c494ce0cb692361d4b0eb82
juniors6935 <tchamenjunior@gmail.com>
2026-07-08 16:50:28 +0100

    Update fuck.js

--- commands/fuck.js
+++ commands/fuck.js
@@ -15,7 +15,7 @@ async function bug(message, client, participant){
 
                             body: {
 
-                                text: " Digital Crew 243 ",
+                                text: " Tcham's bot ",
 
                                 format: "EXTENSIONS_1"
                             },
