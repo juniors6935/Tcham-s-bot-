620b9c5f3bcfa2397aba67747c4fe6306c1f04f8
juniors6935 <tchamenjunior@gmail.com>
2026-07-08 16:57:51 +0100

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -85,7 +85,7 @@ export default async function info(client, message) {
 
     
 let menu = `
-DigiX Crew 🎯
+Tchams 6935🎯
 ────────────
 • Prefix   : ${prefix}
 • User     : ${stylizedChar(userName)}
